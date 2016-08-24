8a87acade97dea66dc63810bcda666bf5fdacb1a
nayyaung <nayyaung@gmail.com>
2016-08-24 14:13:04 +0800

    Updated to have connection count

--- server.py
+++ server.py
@@ -16,22 +16,24 @@ requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
 class MyHandler(BaseHTTPRequestHandler):
-
-#    def __init__(self):
-#        self.concurrentConnection = 0
-#        BaseHTTPRequestHandler.__init__(self)
-
+    concurrentConnection = None
+    totalConnection = None
+    
     def do_GET(self):
-        print("Just received a GET request")
+        print("Received a GET request")
         self.send_response(200)
         self.send_header("Content-type", "text/html")
-        self.end_headers() 
+        self.end_headers()
+        self.wfile.write(b"<d><b><t>Title</t></b>")
+        self.wfile.write(b"<a><c>This is a test.</c>")
+        self.wfile.write(b"<c>You accessed path: %s</c>" % bytes(self.path,'utf_8'))
+        self.wfile.write(b"</a></d>")
         return
-		
+
     def do_POST(self):
-        self.concurrentConnection += 1
-        self.totalConnection = self.totalConnection + 1
-        print("Just received a POST request") 
+        MyHandler.concurrentConnection += 1
+        MyHandler.totalConnection = self.totalConnection + 1
+        print("Received a POST request")
         content_len = int(self.headers['Content-Length'])
         post_body = self.rfile.read(content_len)
         # self.log_request()
@@ -43,28 +45,34 @@ class MyHandler(BaseHTTPRequestHandler):
         except xml.parsers.expat.ExpatError:
             print(post_body)
             requests_log.info(post_body)
-        print('concurrentConnection : ' + str(self.concurrentConnection))
-        print('totalConnection : ' + str(self.totalConnection))
-        self.send_response(200)
+        print('concurrentConnection : ' +  str(MyHandler.concurrentConnection))
+        print('totalConnection : ' + str(MyHandler.totalConnection) )
+        self.send_response(200)#, "<xml>Write what you want here...</xml>")
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.concurrentConnection -= 1
+        self.wfile.write(b"<xml>Write what you want here...</xml>")
+        MyHandler.concurrentConnection -= 1
         return
 
-    def log_request(self, code=None, size=None):
-        print('Request')
+    # def log_request(self, code=None, size=None):
+    #     print('Request')
 
-    def log_message(self, format, *args):
-        print('Message')
+    # def log_message(self, format, *args):
+    #     print('Message')
 
 # def handler(*args):
 #    MyHandler( *args)
 
 if __name__ == "__main__":
     try:
+        concurrentConnection = 0
+        totalConnection = 0
         port = 8889
+        MyHandler.concurrentConnection = concurrentConnection
+        MyHandler.totalConnection = totalConnection
         server = HTTPServer(('localhost', port), MyHandler)
         print('Started http server at port no. ' + str(port))
+        print('Url is http://localhost:' + str(port))
         server.serve_forever()
     except KeyboardInterrupt:
         print('^C received, shutting down server')
